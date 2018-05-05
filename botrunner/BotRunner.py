f9513e10e0815bd5a2446f79f3377df5cf3402ca
dream-admin <killdead.v@gmail.com>
2018-05-05 19:26:29 +0300

    made structure

--- botrunner/BotRunner.py
+++ botrunner/BotRunner.py
@@ -1,7 +1,10 @@
+import os
+import logging
+import i18n
 from flask import Flask, request, Response
+
 from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
-from viberbot.api.messages import KeyboardMessage, RichMediaMessage
 from viberbot.api.messages.text_message import TextMessage
 from viberbot.api.viber_requests import ViberConversationStartedRequest
 from viberbot.api.viber_requests import ViberFailedRequest
@@ -9,14 +12,8 @@ from viberbot.api.viber_requests import ViberMessageRequest
 from viberbot.api.viber_requests import ViberSubscribedRequest
 from viberbot.api.viber_requests import ViberUnsubscribedRequest
 
-import i18n
-import os
+from messageswitch import MessageSwitcher
 
-import time
-import logging
-import sched
-import threading
-import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -25,62 +22,53 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-i18n.load_path.append(os.path.dirname(__file__))
-i18n.set('file_format', 'json')
-i18n.set('locale', 'uk')
 
-app = Flask(__name__)
-viber = Api(BotConfiguration(
-    name='KmBot',
-    avatar='http://viber.com/avatar.jpg',
-    auth_token='47bcc3f5e867d0b6-fdfcaa277e7a2a95-e98ca8adc6516d78'
-))
+def main():
+    i18n.load_path.append(os.path.dirname(__file__))
+    i18n.set('file_format', 'json')
+
+    token = None
+    with open(os.path.dirname(__file__) + '/token_file', 'r') as tokenFile:
+        token = tokenFile.readline()
 
 
-@app.route('/47bcc3f5e867d0b6-fdfcaa277e7a2a95-e98ca8adc6516d78', methods=['POST'])
-def incoming():
-    logger.debug("received request. post data: {0}".format(request.get_data()))
+    app = Flask(__name__)
+    viber = Api(BotConfiguration(
+        name=i18n.t(__get_res('botName')),
+        avatar=i18n.t(__get_res('image-url')),
+        auth_token=token
+    ))
 
-    viber_request = viber.parse_request(request.get_data().decode('utf-8'))
-    if isinstance(viber_request, ViberMessageRequest):
-        message = viber_request.message
-        # lets echo back
-        userId = viber_request.sender.id
-        sendMessage(userId)
-#        viber.send_messages(viber_request.sender.id, [message])
-    elif isinstance(viber_request, ViberSubscribedRequest):
-        viber.send_messages(viber_request.get_user.id, [
-            TextMessage(text="thanks for subscribing!")
-        ])
-    elif isinstance(viber_request, ViberFailedRequest):
-        logger.warn("client failed receiving message. failure: {0}".format(viber_request))
+    messageSwitcher = MessageSwitcher(viber)
 
-    return Response(status=200)
 
-def sendMessage(senderId):
-    startup_keyboard = {
-        "Type": "keyboard",
-        "Buttons": [{
-                    "Columns": 6,
-                    "Rows": 1,
-                    "BgColor": "#2db9b9",
-                    "BgMediaType": "gif",
-                    "BgMedia": "http://www.url.by/test.gif",
-                    "BgLoop": True,
-                    "ActionType": "open-url",
-                    "ActionBody": "www.tut.by",
-                    "Image": "www.tut.by/img.jpg",
-                    "Text": "Key text",
-                    "TextVAlign": "middle",
-                    "TextHAlign": "center",
-                    "TextOpacity": 60,
-                    "TextSize": "regular"
-                    }]
+    @app.route('/' + token, methods=['POST'])
+    def incoming():
+        logger.debug("received request. post data: {0}".format(request.get_data()))
 
-    }
-    viber.send_messages(senderId, [TextMessage(text="thanks for subscribing!", keyboard=startup_keyboard)])
-    logger.debug("message send");
+        viber_request = viber.parse_request(request.get_data().decode('utf-8'))
+        messageSwitcher.disassembleReq(viber_request)
 
+    #     if isinstance(viber_request, ViberMessageRequest):
+    #         message = viber_request.message
+    #         # lets echo back
+    #         userId = viber_request.sender.id
+    #         sendMessage(viber, userId)
+    # # viber.send_messages(viber_request.sender.id, [message])
+    #     elif isinstance(viber_request, ViberSubscribedRequest):
+    #         viber.send_messages(viber_request.get_user.id, [
+    #             TextMessage(text="thanks for subscribing!")
+    #         ])
+    #     elif isinstance(viber_request, ViberFailedRequest):
+    #         logger.warn("client failed receiving message. failure: {0}".format(viber_request))
+
+        return Response(status=200)
+
+    app.run()
+
+
+def __get_res(message):
+    return 'botrunner.' + message
 
 if __name__ == "__main__":
-    app.run()
+    main()
